49c60073d766a5d5edd83520d80548d17456fc4d
aLieexe <rizky.alay@gmail.com>
2025-11-17 13:54:11 +0700

    change app for more clarity

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
@@ -15,18 +14,28 @@ func main() {
 			http.NotFound(w, req)
 			return
 		}
-		_, err := fmt.Fprintf(w, "TEST1234567")
+
+		w.Header().Set("Content-Type", "text/html")
+		_, err := fmt.Fprintln(w, `
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<title>UJK BNSP</title>
+			</head>
+			<body>
+				<h1>RIZKY ALAY HERMAWAN</h1>
+				<p>Deployment pada AWS EC2</p>
+			</body>
+			</html>
+		`)
 		if err != nil {
 			log.Println("error writing response:", err)
 		}
 	})
 
 	server := &http.Server{
-		Addr:         ":5500",
-		Handler:      mux,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  60 * time.Second,
+		Addr:    ":5500",
+		Handler: mux,
 	}
 
 	log.Println("Starting server on :5500")
